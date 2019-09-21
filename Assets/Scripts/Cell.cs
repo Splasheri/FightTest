fd1f61da58e1ea67549621fc33990d2ef81bcbbf
Splasheri <34542678+Splasheri@users.noreply.github.com>
2019-09-22 01:23:05 +0500

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    empty,
+    explored,
+    ally,
+    action,
+    enemy
+}
+
+public class Cell
+{
+    private State state;
+    public  State GetState {get {return state; } }
+
+    private int row, col;
+    public int x { get { return col; } }
+    public int y { get { return row; } }
+
+    private Action onStateChange;
+
+    public Cell(State _state, int x, int y)
+    {
+        state = _state;
+        row = y;
+        col = x;
+    }
+
+    public void ChangeState(State _state)
+    {
+        state = _state;
+        if (onStateChange!=null)
+        {
+            onStateChange();
+            onStateChange = null;
+        }
+    }
+
+    public void SetAction(Action action)
+    {
+        onStateChange = action;
+    }
+    
+    public void ChangePosition(int x, int y)
+    {
+        this.col = x;
+        this.row = y;
+    }
+}
---
+++ Assets/Scripts/SliderProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderProxy : MonoBehaviour
+{
+    public Slider slider;
+    public int Value { get { return SliderValue(); } }
+
+    private void Awake()
+    {
+        OnChange();
+    }
+
+    public void OnChange()
+    {
+        this.GetComponent<Text>().text = slider.value.ToString();
+    }
+
+    private int SliderValue()
+    {
+        return int.Parse(this.GetComponent<Text>().text);
+    }
+}
